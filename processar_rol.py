025cf31ceee5fff9e07ca69ddd2b41235fb5ba22
dnvinicius00@gmail.com <dnvinicius00@gmail.com>
2025-04-01 19:58:02 -0300

    implementação de substituições e melhoria de tratamento de exceção

--- processar_rol.py
+++ processar_rol.py
@@ -7,28 +7,45 @@ pdf_file = "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
 csv_file = "dados_extraidos.csv"
 zip_file = "Teste_Daniel.zip"
 
+substituicoes = {
+    "OD": "Outros Procedimentos",
+    "AMB": "Ambulatório"
+}
+
 dados_tabelas = []
 
-with pdfplumber.open(pdf_file) as pdf:
-    for i, page in enumerate(pdf.pages):
-        print(f"Verificando página {i+1}")
-        tabela = page.extract_table()
-        if tabela:
-            print(f"Tabela encontrada na página {i+1}")
-            dados_tabelas.extend(tabela)
+try:
+    with pdfplumber.open(pdf_file) as pdf:
+        for i, page in enumerate(pdf.pages):
+            tabela = page.extract_table()
 
-if dados_tabelas:
-    with open(csv_file, mode="w", newline="", encoding="utf-8") as arquivo_csv:
-        escritor = csv.writer(arquivo_csv)
-        escritor.writerows(dados_tabelas)
+            if tabela:
+                print(f"Tabela encontrada na página {i+1}")
 
-    print(f"Dados salvos em {csv_file}")
+                tabela_formatada = [
+                    [substituicoes.get(celula.strip() or "", celula.strip()) for celula in linha]
+                    for linha in tabela
+                ]
 
-    with zipfile.ZipFile(zip_file, "w", zipfile.ZIP_DEFLATED) as arquivo_zip:
-        arquivo_zip.write(csv_file)
-    
-    print(f"Arquivo compactado salvo como {zip_file}")
+                dados_tabelas.extend(tabela_formatada)
 
-    os.remove(csv_file)
-else:
-    print("Nenhuma tabela foi encontrada no PDF.")
+    if dados_tabelas:
+        with open(csv_file, mode="w", newline="", encoding="utf-8") as arquivo_csv:
+            escritor = csv.writer(arquivo_csv, delimiter=";")
+            escritor.writerows(dados_tabelas)
+
+        print(f"Dados salvos em {csv_file}")
+
+        with zipfile.ZipFile(zip_file, "w", zipfile.ZIP_DEFLATED) as arquivo_zip:
+            arquivo_zip.write(csv_file)
+
+        print(f"Arquivo compactado salvo como {zip_file}")
+
+        os.remove(csv_file)
+    else:
+        print("Nenhuma tabela foi encontrada no PDF.")
+
+except FileNotFoundError:
+    print(f"Erro: O arquivo {pdf_file} não foi encontrado.")
+except Exception as e:
+    print(f"Erro inesperado: {e}")
